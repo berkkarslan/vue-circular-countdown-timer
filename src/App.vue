1ec4ae86c44aaae670818ea9f234bd74cf5b2b3f
berkarslan <berkarslan97@gmail.com>
2019-10-31 02:18:27 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import VueCountdown from './components/VueCountdown.vue'
+export default {
+  components: {
+    VueCountdown
+  }
+}
+</script>
+
+<template>
+  <vue-countdown></vue-countdown>
+</template>
+
+<style scoped>
+
+</style>
